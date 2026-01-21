da362422c004919a1ac2d7a6538e9bd4ae65e3a2
paiktmddnjs <paiktmddnjs@naver.com>
2026-01-21 16:23:05 +0900

    포인트몰, 관리자의 기프티콘 관리 구현

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,8 +1,10 @@
 import { create } from 'zustand';
 import { createAuthSlice } from './slices/createAuthSlice';
+import { createShopSlice } from './slices/createShopSlice';
 
 const useStore = create((set, get) => ({
-    ...createAuthSlice(set, get)
+    ...createAuthSlice(set, get),
+    ...createShopSlice(set, get)
 }));
 
 export default useStore;
